999879618f40f4b87b4f2352cdb565ae56753fe2
Divyanshi thapa <newone1804@gmail.com>
2025-08-30 18:31:37 +0530

    Update c_loop.sh

--- Arrays/c_loop.sh
+++ Arrays/c_loop.sh
@@ -1,3 +1,4 @@
+```
 #!/bin/bash
 
 
@@ -5,3 +6,5 @@ for (( Jam=1; Jam<=5; Jam++ ))
 do
   echo "Jam: $i"
 done
+```
+![CODE](https://github.com/boa3444/Linux_Lab/blob/bfe7bc407a8f8e8ffb0730b8235a6e3c1da2f7b9/images/array_c_loop.png)
