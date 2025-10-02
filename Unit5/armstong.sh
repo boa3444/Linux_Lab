c99837c86632b1fc5f50c95d1f986120802c9aa4
boa3444 <newone1804@gmail.com>
2025-10-02 18:05:28 +0000

    Scrap

--- Unit5/armstong.sh
+++ Unit5/armstong.sh
@@ -1,55 +1,44 @@
 #!/bin/bash
 
-if [ $# -ne 1 ];
-	then echo "Please input only one input."
+if [ $# -ne 1 ];then
+	echo "Input 1 number only"
 	exit 1
 fi
 
-
-
-num="$1"
-if ! [[ "$num" =~ ^[0-9]+$ ]]; then
-	echo "Your input did not included only integers."
-	exit 1
-fi
-
-
-tumbler="$num"
-length=0
-while [ "$tumbler" -gt 0 ];
-	do tumbler=$((tumbler/10))
-	length=$((length+1))
-done
-
-tumbler="$num"
+num=$1
+remainder=0
+cube=0
 sum=0
-	
-while [ "$tumbler" -gt 0 ];
-	do l_digit=$((tumbler%10))
-	temp_tumbler="$l_digit" 
-
-
-	for ((i= 0; i < (length-1) ; i++)) ;
-		do l_digit=$((l_digit * temp_tumbler))
-		
-	done	
-	sum=$((sum + l_digit))
-	tumbler=$((tumbler/10))
-	
-done
-
-
-if [ "$sum" -eq "$num" ]; then
-	echo "Its an Armstrong number"
-	
-else
-	echo "Its not an Armstrong number"
-	
+digit=0
+org_num=$num
+if ! [[ $num =~ ^[0-9]+$ ]]; then
+	echo "Input numbers only"
+	exit 1
 fi
 
 
 
+while [ $num -ne 0 ];
+do
+	digit=$((digit+1))
+	num=$((num/10))
+	
+done 
 
 
+while [ $org_num -ne 0 ];do
+	remainder=$((org_num%10))
+	cube=$((remainder**3))
+	sum=$((sum+cube))
+	org_num=$((org_num/10))
 
+done
+
+if [ $sum -eq $1 ]; then
+	echo "ANGSTROM"
+
+else 	
+	echo "NOT AN ANGSTROM"
+	
+fi
 
