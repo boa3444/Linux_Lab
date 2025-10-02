54e66f72d02939d5d2b5e585cd593810e6c30164
boa3444 <newone1804@gmail.com>
2025-10-02 18:16:09 +0000

    Practice

--- Unit6/calc.sh
+++ Unit6/calc.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-
+numb1=$1
+numb2=$2
+operator=$3
 result=0
-calc() {
-	num1=$1
-	num2=$2
-	num3=$3
-	if [[ $num3 == / ]];then
-		result=$((num1/num2))
-		echo $result
-		exit 1
-	
-	elif [[ $num3 == + ]];then
-		result=$((num1+num2))
-		echo $result
-		exit 1
-	
-	elif [[ $num3 == - ]];then
-		result=$((num1-num2))
-		echo $result
-		exit 1
-	
-	elif [[ "$num3" == * ]];then
-		result=$((num1*num2))
-		echo $result
+calc(){
+	numb1=$1
+	numb2=$2 #locals
+	operator=$3
+	if [[ $numb2 -eq 0  &&  $operator == / ]];then
+		echo "Invalid input for division"
 		exit 1
 	fi
-	}
+	if [[ $operator == / ]];then
+		result=$((numb1/numb2))
+		echo "$result"
+		exit 1
+	fi
+	if [[ $operator == + ]];then
+		result=$((numb1+numb2))
+		echo "$result"
+		exit 1
+	fi
+	if [[ $operator == * ]];then
+		result=$((numb1*numb2))
+		echo "$result"
+		exit 1
+	fi
+	if [[ $operator == - ]];then
+		result=$((numb1-numb2))
+		echo "$result"
+		exit 1
+	fi
+
+}
 
 calc $1 $2 $3
-		
-
